e82926a59ddd16f0d71da2a7779501fe2b6bd488
TheCodeCumber <ferdinand.ruska@gmail.com>
2023-02-28 19:12:16 +0100

    added .gitignore ... again

--- .gitignore/build/CMakeFiles/FootballManager.dir/src/Object.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/FootballManager.dir/src/Object.cpp.o: \
- /home/ferdinand/CPP/FootballManager/src/Object.cpp \
- /usr/include/stdc-predef.h \
- /home/ferdinand/CPP/FootballManager/include/Object.h
--- .gitignore/build/CMakeFiles/FootballManager.dir/src/Player.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/FootballManager.dir/src/Player.cpp.o: \
- /home/ferdinand/CPP/FootballManager/src/Player.cpp \
- /usr/include/stdc-predef.h \
- /home/ferdinand/CPP/FootballManager/include/Player.h
